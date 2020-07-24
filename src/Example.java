fa502f2ddf292d142254f31172014381c7115360
garm39 <65437547+garm39@users.noreply.github.com>
2020-07-24 17:47:08 +0300

    1.0
    Первая программа на Java

---
+++ src/Example.java
@@ -0,0 +1,12 @@
+/*
+   Это пример простой программы на Java.
+
+   Присвойте файлу с ичсходным кодом имя Exemple.java.
+
+ */
+public class Example {
+    // Выполнения прграммы на Java начинаеться с вызова  метода main()
+    public  static  void main (String argas []) {
+        System.out.println(" Java правит Интернетом! ");
+    }
+}
